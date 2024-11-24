053a1449e629a0edd1e352d565a6417dc458585f
mdsajalali <mdsajalali2020@gmail.com>
2024-11-24 17:51:19 +0600

    fix: footer ui

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -9,8 +9,8 @@ export default function Footer() {
     <footer>
       <div className="bg-[#1D1D1D] text-white">
         <Container>
-          <div className="flex flex-col items-center justify-between py-[55px] md:flex-row">
-            <div className="mb-8 max-w-full md:mb-0 md:max-w-[411px]">
+          <div className="flex flex-col justify-between py-[55px] lg:flex-row lg:items-center lg:gap-10">
+            <div className="max-w-full md:max-w-[411px]">
               {/* Logo */}
               <Link href="/">
                 <Image
@@ -26,7 +26,7 @@ export default function Footer() {
                 est consectetur maxime quos fugit velit assumenda est.
               </p>
             </div>
-            <div className="mb-8 max-w-full md:mb-0 md:max-w-[628px]">
+            <div className="max-w-full md:max-w-[628px]">
               <h1 className="text-2xl font-bold">
                 Sign Up For Our Newsletter!
               </h1>
@@ -34,59 +34,62 @@ export default function Footer() {
                 Get notified about updates and be the first to get early access
                 to new Podcast episodes.
               </p>
-              <div className="flex flex-col items-center md:flex-row">
+              <div className="flex items-center">
                 <input
                   type="email"
                   placeholder="Your email address"
-                  className="mb-4 w-full rounded-l-lg border border-gray-300 px-4 py-2 text-black focus:outline-none md:mb-0 md:w-[435px]"
+                  className="w-full rounded-l-lg border border-gray-300 px-4 py-2 text-black focus:outline-none md:mb-0 md:w-[435px]"
                 />
-                <button className="w-full rounded-r-lg bg-primary px-6 py-[9px] text-white focus:outline-none md:w-[193px]">
+                <button className="w-[120px] rounded-r-lg bg-primary px-6 py-[9px] text-white focus:outline-none md:w-[150px]">
                   Subscribe
                 </button>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-between gap-5 pb-[77px] md:flex-row">
-            <div className="mb-4 text-center md:mb-0 md:text-left">
-              <h1>Contact Us</h1>
-              <p>support@we5ive.com</p>
+          <div className="flex flex-col items-start justify-between gap-5 lg:pb-[77px] pb-10 md:flex-row">
+            <div className="mb-4 md:mb-0 md:text-left">
+              <h1 className="text-[16px] font-medium">Contact Us</h1>
+              <p className="pt-4 text-[15px] font-medium">support@we5ive.com</p>
             </div>
-            <div className="mb-4 list-none text-center md:mb-0 md:text-left">
-              <li>
+            <div className="mb-4 list-none md:mb-0 md:text-left">
+              <li className="text-[16px] font-medium">
                 <a href="#">About Us</a>
               </li>
-              <li>
+              <li className="pt-2 text-[16px] font-medium">
                 <a href="#">Contact Us</a>
               </li>
             </div>
-            <div className="mb-4 list-none text-center md:mb-0 md:text-left">
-              <li>
+            <div className="mb-4 list-none md:mb-0 md:text-left">
+              <li className="text-[16px] font-medium">
                 <a href="#">Privacy policy</a>
               </li>
-              <li>
+              <li className="pt-2 text-[16px] font-medium">
                 <a href="#">Terms & Condition</a>
               </li>
             </div>
-            <div className="text-center md:text-left">
-              <h1>Social Links</h1>
-              <div className="flex items-center justify-center gap-2 md:justify-start">
+            <div className="md:text-left">
+              <h1 className="text-[16px] font-bold">Social Links</h1>
+              <div className="flex items-center justify-center gap-6 pt-6 md:justify-start">
                 <Image
                   src="/social_icons/facebook.svg"
                   alt="facebook"
                   width={20}
                   height={20}
+                  className="cursor-pointer"
                 />
                 <Image
                   src="/social_icons/twitter.svg"
                   alt="facebook"
                   width={20}
                   height={20}
+                  className="cursor-pointer"
                 />
                 <Image
                   src="/social_icons/linkedin.svg"
                   alt="facebook"
                   width={20}
                   height={20}
+                  className="cursor-pointer"
                 />
               </div>
             </div>
