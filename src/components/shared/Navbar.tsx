b15bcf8580dbc59dae8a61829706dfc1ac1609c9
mdsajalali <mdsajalali2020@gmail.com>
2024-11-24 15:52:36 +0600

    fix: navbar ui

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,124 +3,142 @@ import { useState } from "react";
 import Image from "next/image";
 import Container from "./Container";
 import Link from "next/link";
-import { Search, ShoppingBag, User, Menu, X } from "lucide-react";
+import { ShoppingBag, X } from "lucide-react";
+
+const NAV_ITEMS = [
+  { label: "Home", href: "/" },
+  { label: "Shop", href: "/shops" },
+  { label: "Deals", href: "/" },
+  { label: "What's New", href: "/" },
+];
 
 export default function Navbar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <nav className="bg-secondary py-5">
+    <nav className="bg-secondary py-3 lg:py-5">
       <Container>
         <div className="flex items-center justify-between">
-          {/* Logo */}
-          <Link href="/">
-            <Image src="/logo.png" alt="logo" width={161} height={100} />
-          </Link>
+          <div className="flex gap-2">
+            {/* Hamburger Menu for Mobile */}
+            <button
+              className="block lg:hidden"
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+            >
+              <Image
+                src="/menu.svg"
+                alt="menu"
+                width={22}
+                height={22}
+                className="ml-2"
+              />
+            </button>
 
-          {/* Hamburger Menu for Mobile */}
-          <button
-            className="block lg:hidden"
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          >
-            {isSidebarOpen ? (
-              <X className="text-gray-700" />
-            ) : (
-              <Menu className="text-gray-700" />
-            )}
-          </button>
+            {/* Logo */}
+            <Link href="/">
+              <Image
+                src="/logo.png"
+                alt="logo"
+                width={161}
+                height={100}
+                className="h-auto w-[110px] lg:w-[161px]"
+              />
+            </Link>
+          </div>
 
-          {/* Navigation Links (Hidden on Mobile) */}
+          {/* Desktop Navigation Links */}
           <div className="hidden lg:block">
-            <ul className="flex items-center gap-4">
-              <Link
-                href="/"
-                className="font-semibold text-[#646464] duration-300 hover:text-primary"
-              >
-                Home
-              </Link>
-              <Link
-                href="/shops"
-                className="font-semibold text-[#646464] duration-300 hover:text-primary"
-              >
-                Shop
-              </Link>
-              <Link
-                href="/"
-                className="font-semibold text-[#646464] duration-300 hover:text-primary"
-              >
-                Deals
-              </Link>
-              <Link
-                href="/"
-                className="font-semibold text-[#646464] duration-300 hover:text-primary"
-              >
-                What's New
-              </Link>
+            <ul className="flex items-center gap-5">
+              {NAV_ITEMS.map((item) => (
+                <li key={item.href}>
+                  <Link
+                    href={item.href}
+                    className="text-[18px] font-semibold text-[#646464] duration-300 hover:text-primary"
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
 
-          {/* Search and Icons (Hidden on Mobile) */}
-          <div className="hidden items-center gap-2 lg:flex">
-            <div className="flex w-[242px] items-center rounded-full bg-white py-2 shadow-sm">
-              <Search className="ml-3 text-gray-500" />
+          {/* Search and Icons */}
+          <div className="justify-end gap-4 lg:flex">
+            <div className="hidden w-[242px] items-center rounded-full bg-white py-2 shadow-sm lg:flex">
+              <Image
+                src="/search.svg"
+                alt="search"
+                width={20}
+                height={20}
+                className="ml-2"
+              />
               <input
                 type="text"
-                className="ml-2 flex-1 bg-transparent text-gray-800 placeholder-gray-400 focus:outline-none"
+                placeholder="Search"
+                className="ml-2 flex-1 bg-transparent text-[16px] placeholder-black focus:outline-none"
               />
             </div>
 
-            <div className="flex items-center gap-2">
-              <Search className="text-gray-500" />
-              <Link href="/cart" className="relative">
+            <div className="flex items-center justify-center gap-4">
+              <Image
+                onClick={() => setIsSidebarOpen(false)}
+                src="/search.svg"
+                alt="search"
+                width={22}
+                height={22}
+                className="ml-2 block lg:hidden"
+              />
+              <Link
+                onClick={() => setIsSidebarOpen(false)}
+                href="/cart"
+                className="relative"
+              >
                 <ShoppingBag className="text-2xl text-gray-700" />
                 <span className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-black text-xs text-white">
                   0
                 </span>
               </Link>
-              <User className="text-gray-500" />
+              <Image
+                onClick={() => setIsSidebarOpen(false)}
+                src="/user.svg"
+                alt="user"
+                width={22}
+                height={22}
+              />
             </div>
           </div>
         </div>
 
-        {/* Sidebar for Mobile */}
-        {isSidebarOpen && (
-          <div className="fixed inset-y-0 right-0 z-50 w-64 bg-white shadow-lg">
-            <div className="p-4">
-              <button
-                className="mb-4 text-gray-700"
-                onClick={() => setIsSidebarOpen(false)}
-              >
-                <X className="h-6 w-6" />
-              </button>
-              <ul className="space-y-4">
-                <Link
-                  href="/"
-                  className="block font-semibold text-[#646464] hover:text-primary"
-                >
-                  Home
-                </Link>
-                <Link
-                  href="/"
-                  className="block font-semibold text-[#646464] hover:text-primary"
-                >
-                  Shop
-                </Link>
-                <Link
-                  href="/"
-                  className="block font-semibold text-[#646464] hover:text-primary"
-                >
-                  Deals
-                </Link>
-                <Link
-                  href="/"
-                  className="block font-semibold text-[#646464] hover:text-primary"
-                >
-                  What's New
-                </Link>
-              </ul>
-            </div>
+        {/* Mobile Sidebar */}
+        <div
+          className={`fixed inset-y-0 left-0 z-50 w-64 bg-secondary shadow-lg transition-transform duration-500 ${
+            isSidebarOpen
+              ? "translate-x-0 transform"
+              : "-translate-x-full transform"
+          }`}
+        >
+          <div className="p-4">
+            <button
+              className="mb-4 ml-auto block text-gray-700"
+              onClick={() => setIsSidebarOpen(false)}
+            >
+              <X className="h-6 w-6" />
+            </button>
+            <ul className="space-y-4">
+              {NAV_ITEMS.map((item) => (
+                <li key={item.href}>
+                  <Link
+                    onClick={() => setIsSidebarOpen(false)}
+                    href={item.href}
+                    className="block text-[15px] font-semibold text-[#646464] hover:text-primary"
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        )}
+        </div>
       </Container>
     </nav>
   );
